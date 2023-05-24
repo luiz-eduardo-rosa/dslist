649869ffd61cc8cff71e9c89a462319edc2799ee
Luiz Eduardo <luizeduardodu@hotmail.com>
2023-05-24 14:38:12 -0300

    Move games on list

--- src/main/java/com/luizeduardo/dslist/services/GameListService.java
+++ src/main/java/com/luizeduardo/dslist/services/GameListService.java
@@ -8,7 +8,9 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.luizeduardo.dslist.dto.GameListDTO;
 import com.luizeduardo.dslist.entities.GameList;
+import com.luizeduardo.dslist.projections.GameMinProjection;
 import com.luizeduardo.dslist.repositories.GameListRepository;
+import com.luizeduardo.dslist.repositories.GameRepository;
 
 
 @Service
@@ -17,9 +19,29 @@ public class GameListService {
     @Autowired
     private GameListRepository gameListRepository;
 
+    @Autowired
+    private GameRepository gameRepository;
+
     @Transactional(readOnly = true)
     public List<GameListDTO> findAll(){
         List<GameList> result = gameListRepository.findAll();
         return result.stream().map(GameListDTO::new).toList();
+    }
+
+    @Transactional
+	public void move(Long listId, int sourceIndex, int destinationIndex) {
+
+		List<GameMinProjection> list = gameRepository.searchByList(listId);
+
+		GameMinProjection obj = list.remove(sourceIndex);
+		list.add(destinationIndex, obj);
+
+		int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+		int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+
+		for (int i = min; i <= max; i++) {
+			gameListRepository.updateBelongingPosition(listId, list.get(i).getId(), i);
+		}
+        
     }
 }
